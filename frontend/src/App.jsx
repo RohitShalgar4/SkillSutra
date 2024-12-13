62d44a830de37838a801d36cf113a91058f84d1f
RohitShalgar4 <rohishalgar04@gmail.com>
2024-12-13 19:55:16 +0530

    added Login for temp testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Button } from './components/ui/button'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <>
-      <Button>Let's Build SkillSutra</Button>
-    </>
+    <main>
+      <Login/>
+    </main>
   )
 }
 
