ece3f1d949c68500d76af7a7ec9dcde0a96b4b5c
RohitShalgar4 <rohishalgar04@gmail.com>
2024-12-13 17:07:14 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <Button>Let's Build SkillSutra</Button>
+    </>
+  )
+}
+
+export default App
