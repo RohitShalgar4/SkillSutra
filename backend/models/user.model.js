bd6648a293bae995d717713cfd4874e75863b37a
RohitShalgar4 <rohishalgar04@gmail.com>
2024-12-14 13:09:44 +0530

    added user schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:
+        {
+            type: String,
+            required: true
+        },
+    email:
+        {
+            type: String,
+            reuired: true
+        },
+    password:
+        {
+                type: String,
+                reuired: true
+        },
+    role:
+        {
+            type: String,
+            enum: ["Instructor", "Student"],
+            default: "Student"
+        },
+    enrolledCourses:
+        [
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref: "Course"
+            }
+        ],
+    photoURL:
+        {
+            type: String,
+            default:""
+        }
+},{timestamp:true});
+
+export const User = mongoose.model("User",userSchema);
