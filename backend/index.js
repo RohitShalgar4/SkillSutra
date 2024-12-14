a0b33a6a98aba6db6d06d5e8089862f686336202
RohitShalgar4 <rohishalgar04@gmail.com>
2024-12-14 13:13:39 +0530

    added api and cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './database/db.js';
+import userRoute from "./routes/user.route.js"
+import cookieParser from 'cookie-parser';
 
 dotenv.config({});
 connectDB();
@@ -8,6 +11,17 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true
+}))
+
+// API
+app.use("/api/v1/user", userRoute);
+
 
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
