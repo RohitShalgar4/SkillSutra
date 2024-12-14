7f8891e4f7af73b15a4d9092e07fdf3a9f063b4e
RohitShalgar4 <rohishalgar04@gmail.com>
2024-12-14 13:16:50 +0530

    added store

---
+++ frontend/src/app/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit" 
+import rootRedcuer from "./rootReducer";
+import { authApi } from "@/features/api/authApi";
+
+export const appStore = configureStore({
+    reducer: rootRedcuer,
+    middleware:(defaultMiddleware) => defaultMiddleware().concat(authApi.middleware)
+});
