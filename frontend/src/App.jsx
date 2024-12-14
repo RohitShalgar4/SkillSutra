418a4ed3db8a9ca6f1470c8a96feeea514775bc8
Udaymore741 <orionlee741@gmail.com>
2024-12-15 04:59:54 +0530

    App.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,40 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Button } from "./components/ui/button";
+import HeroSection from "./pages/Student/heroscetion";
 import Login from "./pages/Login";
+import MainLayout from "./Layout/mainlayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Courses from "./pages/Student/courses";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <HeroSection />
+            <Courses/>
+          </>
+        ),
+      },
+      {
+        path: "login",
+        element: <Login />
+      }
+    ],
+  },
+]);
+
 
 function App() {
+
   return (
     <main>
-      <Navbar />
-      <Login />
+      <RouterProvider router={appRouter} />
     </main>
   );
 }
