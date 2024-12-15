a51cf09cddd6a848103e2b8a8daf03edfb540722
vaibhavibelamkar <vaibhavi.belamkar22@gmail.com>
2024-12-16 00:20:00 +0530

    store.js modified

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,17 @@
-import {configureStore} from "@reduxjs/toolkit" 
+import { configureStore } from "@reduxjs/toolkit";
 import rootRedcuer from "./rootReducer";
 import { authApi } from "@/features/api/authApi";
+import { courseApi } from "@/features/api/courseApi";
 
 export const appStore = configureStore({
-    reducer: rootRedcuer,
-    middleware:(defaultMiddleware) => defaultMiddleware().concat(authApi.middleware)
+  reducer: rootRedcuer,
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware().concat(authApi.middleware, courseApi.middleware),
 });
 
 const intializeApp = async () => {
-    await appStore.dispatch(authApi.endpoints.loadUser.initiate({},{forceRefetch:true}));
-}
+  await appStore.dispatch(
+    authApi.endpoints.loadUser.initiate({}, { forceRefetch: true })
+  );
+};
 intializeApp();
