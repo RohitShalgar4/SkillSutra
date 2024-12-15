1b0609c3fdb894e20302bf8aa4dfab17e54801b9
Bhargav Govind Katkam <bhargavkatkam0@gmail.com>
2024-12-15 20:59:18 +0530

    Updated User UI

--- frontend/src/features/api/authApi.js
+++ frontend/src/features/api/authApi.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { userLoggedIn } from "../authSlice";
+import { userLoggedIn, userLoggedOut } from "../authSlice";
 
 const USER_API = "http://localhost:8080/api/v1/user/";
 
@@ -32,11 +32,33 @@ export const authApi = createApi({
         }
       },
     }),
+    logoutUser: builder.mutation({
+      query: () => ({
+        url: "logout",
+        method:"GET"
+      })
+    }),
+    async onQueryStarted(_, { queryFulfilled, dispatch }) {
+      try {
+        const result = await queryFulfilled;
+        dispatch(userLoggedOut);
+      } catch (error) {
+        console.log(error);
+      }
+    },
     loadUser: builder.query({
       query: () => ({
         url: "profile",
         method: "GET",
       }),
+      async onQueryStarted(_, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(userLoggedIn({ user: result.data.user }));
+        } catch (error) {
+          console.log(error);
+        }
+      },
     }),
     updateUser: builder.mutation({
       query: (formData) => ({
@@ -51,6 +73,7 @@ export const authApi = createApi({
 export const {
   useRegisterUserMutation,
   useLoginUserMutation,
+  useLogoutUserMutation,
   useLoadUserQuery,
   useUpdateUserMutation,
 } = authApi;
