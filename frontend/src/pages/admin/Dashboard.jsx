6425f6a003d131f5fd0d8a84d1f766bab642a0eb
vaibhavibelamkar <vaibhavi.belamkar22@gmail.com>
2024-12-16 00:22:41 +0530

    Dashboard.jsx added

---
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,38 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+      <Card className="shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <CardHeader>
+          <CardTitle>Total Sales</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-3xl font-bold text-blue-600"></p>
+        </CardContent>
+      </Card>
+
+      <Card className="shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <CardHeader>
+          <CardTitle>Total Revenue</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-3xl font-bold text-blue-600"></p>
+        </CardContent>
+      </Card>
+
+      {/* Course Prices Card */}
+      <Card className="shadow-lg hover:shadow-xl transition-shadow duration-300 col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-4">
+        <CardHeader>
+          <CardTitle className="text-xl font-semibold text-gray-700">
+            Course Prices
+          </CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Dashboard;
