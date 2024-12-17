e97156a61a0b355f0e05cdc72ca841074e8abe18
Bhargav Govind Katkam <bhargavkatkam0@gmail.com>
2024-12-17 20:04:32 +0530

    Updated the file name for Courses

--- frontend/src/pages/Student/courses.jsx
+++ frontend/src/pages/Student/courses.jsx
@@ -1,20 +1,27 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import Course from "./Course";
 import React from "react";
+import { useGetPublishedCourseQuery } from "@/features/api/courseApi";
+import { Link } from "react-router-dom";
 
 const Courses = () => {
-  const courses = [1, 2, 3, 4, 5, 6, 7, 8];
-  const isLoading = false;
+  const {data, isLoading, isError} = useGetPublishedCourseQuery();
+
+  if(isError) return <h1>Some error occurred while fetching courses.</h1>
+
+
   return (
-    <div classname="bg-gray-50">
+    <div className="bg-gray-50 dark:bg-[#141414]">
       <div className="max-w-7xl mx-auto p-6">
         <h2 className="font-bold text-3xl text-center mb-10">Our Courses</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {isLoading
-            ? Array.from({ length: 8 }).map((_, index) => (
-                <CourseSkeleton key={index} />
-              ))
-            : courses.map((course, index) => <Course key={index} />)}
+          {isLoading ? (
+            Array.from({ length: 8 }).map((_, index) => (
+              <CourseSkeleton key={index} />
+            ))
+          ) : (
+           data?.courses && data.courses.map((course, index) => <Course key={index} course={course}/>) 
+          )}
         </div>
       </div>
     </div>
