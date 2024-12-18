7ab2ad90679d1f86aa0d4107098671b5b2ca78e3
RohitShalgar4 <rohishalgar04@gmail.com>
2024-12-18 23:01:40 +0530

    added routes for purchase and progress

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
 
 dotenv.config({});
 connectDB();
@@ -29,6 +30,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listen at port ${PORT}`);
