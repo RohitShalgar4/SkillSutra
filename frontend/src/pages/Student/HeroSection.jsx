5377b705ab27984f6e13f94ffb9f7b9314771ab9
vaibhavibelamkar <vaibhavi.belamkar22@gmail.com>
2024-12-19 13:06:13 +0530

    Search Handler is added in herosection

--- frontend/src/pages/Student/HeroSection.jsx
+++ frontend/src/pages/Student/HeroSection.jsx
@@ -1,6 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+  const searchHandler = (e) => {
+    e.preventDefault();
+    if (searchQuery.trim() !== "") {
+      navigate(`/course/search?query=${searchQuery}`);
+    }
+    setSearchQuery("");
+  };
   return (
     <div className="relative bg-gradient-to-r from-blue-500 to-indigo-600 dark:from-gray-800 dark:to-gray-900 py-20 px-4 text-center w-full">
       <div className="max-w-3xl mx-auto">
@@ -11,11 +23,13 @@ const HeroSection = () => {
           Discover, Learn, and Upskill with our wide range of courses
         </p>
         <form
-          action=""
+          onSubmit={searchHandler}
           className="flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mb-6"
         >
-          <input
+          <Input
             type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Search Courses"
             className="flex-grow border-none focus-visible:ring-0 px-6 py-3 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500"
           />
