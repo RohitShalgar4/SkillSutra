05173673ddfc37f7a18b60058a2593c095c506ee
Udaymore741 <orionlee741@gmail.com>
2025-02-12 16:31:00 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
+import certificateRouter from "./routes/certificate.route.js";
 
 dotenv.config({});
 connectDB();
@@ -31,6 +32,7 @@ app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
+app.use("/api/v1/certificates", certificateRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listen at port ${PORT}`);
