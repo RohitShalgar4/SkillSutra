ffcf4922ce7f71746bd152f54a655f1df9ad995a
Udaymore741 <orionlee741@gmail.com>
2025-02-12 16:32:30 +0530

    added api for certificate

---
+++ frontend/src/features/api/certificateApi.js
@@ -0,0 +1,27 @@
+// features/api/certificateApi.js
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const CERTIFICATE_API = "http://localhost:8080/api/v1/certificates/";
+
+export const certificateApi = createApi({
+    reducerPath: "certificateApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: CERTIFICATE_API,
+        credentials: "include",
+        prepareHeaders: (headers) => {
+            headers.set('Accept', 'application/pdf');
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        generateCertificate: builder.mutation({
+            query: (courseId) => ({
+                url: `${courseId}/generate`,
+                method: "POST",
+                responseHandler: async (response) => response.blob(),
+            }),
+        }),
+    }),
+});
+
+export const { useGenerateCertificateMutation } = certificateApi;
